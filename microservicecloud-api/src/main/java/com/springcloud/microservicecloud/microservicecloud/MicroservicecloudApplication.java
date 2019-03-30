a283bb9046a64625d10e234e8e1e2b9a9b2e3013
starwu <starwu@atguigu.com>
2019-03-30 18:05:18 +0800

    microservicecloud

---
+++ microservicecloud-api/src/main/java/com/springcloud/microservicecloud/microservicecloud/MicroservicecloudApplication.java
@@ -0,0 +1,13 @@
+package com.springcloud.microservicecloud.microservicecloud;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MicroservicecloudApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MicroservicecloudApplication.class, args);
+	}
+
+}
---
+++ microservicecloud-api/src/main/java/com/springcloud/microservicecloud/microservicecloud/contoller/IndexController.java
@@ -0,0 +1,11 @@
+package com.springcloud.microservicecloud.microservicecloud.contoller;
+
+import org.springframework.stereotype.Controller;
+
+@Controller
+public class IndexController {
+
+    public static void main(String[] args) {
+        System.out.println("dkhfjsdlfjsd");
+    }
+}
